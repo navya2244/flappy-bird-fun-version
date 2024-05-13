e3e633fe501fafc7433e558aa0bbdd3cc58bff9f
navya2244 <145574691+navya2244@users.noreply.github.com>
2024-05-13 09:29:47 -0400

    Add files via upload
    I attempted making the bird move but didn't get so far.

--- bird.py
+++ bird.py
@@ -10,5 +10,9 @@ class Bird:
     self.delta = 2
     self.up = True
     self.down = False
-
-def
+  def move(self):
+    if self.up:
+      self.y -= self.delta
+    if self.down:
+      self.y += self.delta
+    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
--- main.py
+++ main.py
@@ -11,13 +11,15 @@ frame = 0
 bird = pygame.image.load("bluebird-midflap.png")
 bird_x = 50
 bird_y = 250
+fps = 32
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             run = False
-   
-
-
+    keys = pygame.key.get_pressed()  # checking pressed keys
+    bird = Bird(bird_x, bird_y)  # Creating an instance of the Bird class
+    if keys[pygame.K_SPACE]:
+        bird.move()  # Calling the move method on the Bird instance
     screen.blit(bg, (0, 0))
     screen.blit(bird,(bird_x,bird_y))
     pygame.display.update()
