3474fc7dee5d24f582d13fcb962cab50424a960e
navya2244 <145574691+navya2244@users.noreply.github.com>
2024-05-16 09:29:01 -0400

    Add files via upload
    changed bird class for more physics and coding the physics in the main.py i think i am going to change the bird class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import pygame
+from pygame.constants import K_SPACE
 from pygame.display import update
 from bird import Bird
 
@@ -7,20 +8,26 @@ pygame.init()
 screen = pygame.display.set_mode((800, 800))
 run = True
 bg = pygame.image.load("background.png")
-frame = 0
-bird = pygame.image.load("bluebird-midflap.png")
-bird_x = 50
-bird_y = 250
+
+bird = Bird(50, 250)
+brid_y = 200
+bird_velocity = 0 
+bird_acceleration = 0.25
+jump_strength = -5 
+
 fps = 32
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             run = False
-    keys = pygame.key.get_pressed()  # checking pressed keys
-    bird = Bird(bird_x, bird_y)  # Creating an instance of the Bird class
-    if keys[pygame.K_SPACE]:
-        bird.move()  # Calling the move method on the Bird instance
+    bird_velocity += bird_acceleration
+    bird.y += bird_velocity
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_SPACE:
+               bird_velocity = jump_strength
+        if 
+    screen.blit(bird.image, bird.rect)
     screen.blit(bg, (0, 0))
-    screen.blit(bird,(bird_x,bird_y))
     pygame.display.update()
 pygame.quit()
