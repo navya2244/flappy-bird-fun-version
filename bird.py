f2b90cdf539245f9c62e33cdc48d23b34f1a5124
navya2244 <145574691+navya2244@users.noreply.github.com>
2024-05-28 09:27:02 -0400

    Add files via upload
    move my bird !!! I changed the bird's move function so it would work and I called it differently in the main.py still more work to be done.

--- bird.py
+++ bird.py
@@ -11,6 +11,9 @@ class Bird:
     self.image_size = self.image.get_size()
     self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1]) 
     self.delta = 3
+    # jump code below
+    self.is_jumping = True
+    self.is_falling = False
     
   def rescale_image(self, image):
 
@@ -18,10 +21,19 @@ class Bird:
     scale_size = (self.image_size[0] * 0.7, self.image_size[1] * 0.7)
     self.image = pygame.transform.scale(self.image,scale_size)
 
-  def move(self, keys_press):
-    if keys_press[pygame.K_SPACE]:
-        self.y -= self.delta
+  def move(self):
+    keys = pygame.key.get_pressed()
+    if keys == pygame.K_SPACE:
+      self.y += self.delta
+      self.x += self.delta
     else:
       self.y -= self.delta
       self.y = max(0, min(self.y, 600 - self.image_size[1]))
       self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
+  
+  def gravity(self):
+    if self.is_jumping:
+        self.y += 3.2
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+                        
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import pygame
-from pygame.constants import K_SPACE
+from pygame.constants import K_0, K_SPACE
 from pygame.display import update
 from bird import Bird
 
@@ -9,26 +9,23 @@ screen = pygame.display.set_mode((800, 800))
 run = True
 bg = pygame.image.load("background.png")
 bird_image = pygame.image.load("bluebird-midflap.png")
-bird = Bird(50, 250)
 bird_y = 200
+bird_x = 50
+bird = Bird(bird_x, bird_y)
 bird_velocity = 0 
 bird_acceleration = 0.25
 jump_strength = -5 
-
+y = bird_y
 fps = 32
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             run = False
-        if event.type == pygame.KEYDOWN:
-            Bird.move(bird, K_SPACE)
-    #     if event.type == pygame.KEYDOWN and event.key == K_SPACE:
-    #         bird_velocity = jump_strength
-    # bird_velocity += bird_acceleration
-    # bird.y += int(bird_velocity)
+        if event.type == pygame.KEYUP:
+                bird.move()
     
     screen.blit(bg, (0, 0))
-    screen.blit(bird.image, (50, 250))    
+    screen.blit(bird.image, bird.rect)    
 
     pygame.display.update()
 pygame.quit()
