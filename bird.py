032902de4ed8401c4b5776e01a2a41fdec17da85
navya2244 <145574691+navya2244@users.noreply.github.com>
2024-06-12 09:22:49 -0400

    Add files via upload
    final commit! jumping

--- bird.py
+++ bird.py
@@ -1,39 +1,34 @@
-import pygame 
+import pygame
 
-class Bird: 
+class Bird:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("bluebird-midflap.png")
+        self.image_size = self.image.get_size()  # Define image_size before rescaling
+        self.rescale_image()  # Call rescale_image after defining image_size
+        self.rect = pygame.Rect(self.x, self.y, self.image.get_width(), self.image.get_height())
+        self.delta = 5
+        self.is_jumping = False
+        self.is_falling = False
+        self.vel_y = 0
+        self.jump_strength = -15
+        self.gravity = 1
 
+    def rescale_image(self):
+        scale_size = (int(self.image_size[0] * 0.7), int(self.image_size[1] * 0.7))
+        self.image = pygame.transform.scale(self.image, scale_size)
 
-  def __init__(self,x,y):
-    self.x = x
-    self.y = y
-    self.image = pygame.image.load("bluebird-midflap.png")
-    self.rescale_image(self.image)
-    self.image_size = self.image.get_size()
-    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1]) 
-    self.delta = 3
-    # jump code below
-    self.is_jumping = True
-    self.is_falling = False
-    
-  def rescale_image(self, image):
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and not self.is_jumping and self.rect.bottom >= 600:
+            self.is_jumping = True
+            self.vel_y = self.jump_strength
 
-    self.image_size = self.image.get_size()
-    scale_size = (self.image_size[0] * 0.7, self.image_size[1] * 0.7)
-    self.image = pygame.transform.scale(self.image,scale_size)
+        self.vel_y += self.gravity
+        self.rect.y += self.vel_y
 
-  def move(self):
-    keys = pygame.key.get_pressed()
-    if keys == pygame.K_SPACE:
-      self.y += self.delta
-      self.x += self.delta
-    else:
-      self.y -= self.delta
-      self.y = max(0, min(self.y, 600 - self.image_size[1]))
-      self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-
-  
-  def gravity(self):
-    if self.is_jumping:
-        self.y += 3.2
-        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-                        
+        if self.rect.bottom >= 600:  # Assuming the ground level is at y = 600
+            self.rect.bottom = 600
+            self.is_jumping = False
+            self.vel_y = 0
--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-
 import pygame
-from pygame.constants import K_0, K_SPACE
-from pygame.display import update
 from bird import Bird
 
 pygame.init()
-screen = pygame.display.set_mode((800, 800))
-run = True
+clock = pygame.time.Clock()
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+base = pygame.image.load("base.png")
 bg = pygame.image.load("background.png")
-bird_image = pygame.image.load("bluebird-midflap.png")
-bird_y = 200
-bird_x = 50
-bird = Bird(bird_x, bird_y)
-bird_velocity = 0 
-bird_acceleration = 0.25
-jump_strength = -5 
-y = bird_y
-fps = 32
+
+bird = Bird(500, 500)
+
+run = True
+
 while run:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
+        if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.KEYUP:
-                bird.move()
-    
-    screen.blit(bg, (0, 0))
-    screen.blit(bird.image, bird.rect)    
 
+    bird.update()
+
+    screen.blit(bg, (0, 0))
+    screen.blit(bird.image, bird.rect)
+ 
     pygame.display.update()
+    clock.tick(60)
+
 pygame.quit()
